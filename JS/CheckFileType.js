2ad0157c0869c0300eb71cb86d544f2611df9ff2
dwivediparas777 <dwivediparas777@users.noreply.github.com>
2019-11-30 14:52:48 +0530

    Create CheckFileType.js

---
+++ JS/CheckFileType.js
@@ -0,0 +1,24 @@
+var _acceptableExtension = [".inkml"];
+function validation(fileNames){
+    var arrInput = fileNames.getElementsByTagName("ink_ml");
+    for(var i=0; i<arrInput.length; i++){
+        var inputData = arrInput[i];
+        if(inputData.type == "file"){
+            var fileName = inputData.value;
+            if (fileName.length > 0) {
+                var blnValid = false;
+                for (var j = 0; j < _acceptableExtension.length; j++) {
+                    var sCurExtension = _acceptableExtension[j];
+                    if (fileName.substr(fileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+                        blnValid = true;
+                        break;
+                    }
+                }
+                if (!blnValid) {
+                    alert("Sorry, " + fileName + " is invalid, allowed extensions are: " + _acceptableExtension.join(", "));
+                    return false;
+                }
+            }
+        }
+    }
+}
