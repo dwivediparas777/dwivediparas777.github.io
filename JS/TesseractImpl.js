f05d5dd876337f586523ff3d360d37bc5134892b
dwivediparas777 <dwivediparas777@users.noreply.github.com>
2020-07-31 14:59:20 +0530

    Update TesseractImpl.js

--- JS/TesseractImpl.js
+++ JS/TesseractImpl.js
@@ -1,4 +1,6 @@
 function doFileTesseract(data){
+    console.log("doFileTesseract")
+    console.log(data)
     Tesseract.recognize(data).then(function(result){
         document.getElementById("textareaOCR").value="Hello world".concat(result.text);
     });
