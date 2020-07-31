000717063797cec902f378c7179fab7b6f31b9f9
dwivediparas777 <dwivediparas777@users.noreply.github.com>
2020-07-31 15:00:33 +0530

    Update CheckFileType.js

--- JS/CheckFileType.js
+++ JS/CheckFileType.js
@@ -1,6 +1,8 @@
 
 function validation(fileNames, _acceptableExtension){
     var arrInput = fileNames.getElementsByTagName("input");
+    console.log("test validation");
+    console.log(fileNames);
     for(var i=0; i<arrInput.length; i++){
         var inputData = arrInput[i];
         if(inputData.type == "file"){
