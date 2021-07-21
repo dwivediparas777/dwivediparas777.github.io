7a1df5baa883efa385cedc90ea88c8903aa261b6
dwivediparas777 <dwivediparas777@users.noreply.github.com>
2021-07-21 20:56:18 +0530

    Delete TesseractImpl.js

--- JS/TesseractImpl.js
+++
@@ -1,7 +0,0 @@
-function doFileTesseract(data){
-    console.log("doFileTesseract")
-    console.log(data)
-    Tesseract.recognize(data).then(function(result){
-        document.getElementById("textareaOCR").value="Hello world".concat(result.text);
-    });
-}
