0177f6bf90d706f5133db401567c3836cd08c138
dwivediparas777 <paraspass123@gmail.com>
2021-07-26 17:53:01 +0530

    Adding commands and CLI view change

--- main_page.js
+++ main_page.js
@@ -1,9 +1,45 @@
 // Group of events and there corresponding event functions
-var events = {"help": trigger_help};
+var events = {"help": [trigger_help, "Shows available list of comands"],
+                "skills": [trigger_get_skills, "My primary skills"],
+                "awards": [trigger_get_awards, "Awards received and there details"],
+                "languages": [trigger_get_languages, "List of languages and fluency"],
+                "education": [trigger_get_education, "My education detail"],
+                "experience": [trigger_get_experience, "Shows experience"]};
 
-function trigger_help(){
+function get_default_message(){
     var message = document.createElement("h4");
     message.innerText = "Coming Soon...";
+    return message;
+}
+
+function trigger_get_education(){
+    return get_default_message();
+}
+
+function trigger_get_experience(){
+    return get_default_message();
+}
+
+function trigger_get_languages(){
+    return get_default_message();
+}
+
+function trigger_get_awards(){
+    return get_default_message();
+}
+
+function trigger_get_skills(){
+    return get_default_message();
+}
+
+function trigger_help(){
+    var message = document.createElement("div");
+    message.style = "margin-left:55px";
+    message.innerHTML = "Please choose from below queries<ul>";
+    for (const [key, value] of Object.entries(events)) {
+        message.innerHTML += "<li>" + key + " : " + value[1] + "</li>";
+    }
+    message.innerHTML += "</ul><br>";
     return message;
 }
 
@@ -12,23 +48,23 @@ function create_new_entry(){
     window.session_block_id = window.session_block_id + 1;
 
     var new_block = document.createElement('div');
+    new_block.style = "display:inline-flex"
 
     var div_block_left = document.createElement('div');
-    div_block_left.style = "width:10%;font-weight:bold";
+    div_block_left.style = "width:2%;font-weight:bold";
     div_block_left.innerText = " > ";
 
     new_block.append(div_block_left);
 
     var div_block_right = document.createElement('div');
+    div_block_right.style = "width:90%";
     var input_block = document.createElement('input');
     input_block.type = 'text';
     input_block.placeholder = "Type help.";
     input_block.className = "inputBox";
     input_block.size = "175";
-    //input_block.id = "helpButton_"+window.session_block_id;
     input_block.id = "helpButton_0";
     div_block_right.append(input_block);
-
     new_block.append(div_block_right);
 
     return new_block;
@@ -47,7 +83,7 @@ function trigger_event(query){
     query = query.toLowerCase();
 
     if(query in events){
-        message = events[query]();
+        message = events[query][0]();
     }
     else{
         message = data_not_found();
@@ -65,8 +101,13 @@ function trigger_event(query){
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 13 && event.target && event.target.id== 'helpButton_0') { 
         var query = document.getElementById("helpButton_0").value;
-        document.getElementById("helpButton_0").id = "helpButton";
-        document.getElementById("helpButton").disabled = true;
-        trigger_event(query);
+        query = query.trim();
+        if(query){
+            document.getElementById("helpButton_0").disabled = true;
+            document.getElementById("helpButton_0").id = "helpButton";
+            document.getElementById("helpButton").disabled = false;
+            trigger_event(query);
+            document.getElementById("helpButton_0").focus();
+        }
     }
 });
