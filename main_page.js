3338c59fc4f855129ba0ac64af7e354cc82dfd8c
dwivediparas777 <paraspass123@gmail.com>
2021-07-27 20:23:05 +0530

    Update the profile photo

--- main_page.js
+++ main_page.js
@@ -111,3 +111,27 @@ document.addEventListener("keyup", function(event) {
         }
     }
 });
+
+
+// Code to update visit count
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+function increaseVisitCount(){
+    readTextFile("data/views.json", function(text){
+        var data = JSON.parse(text);
+        console.log("increaseVisitCount : ", data);
+    });
+
+}
+
+window.onload = increaseVisitCount();
